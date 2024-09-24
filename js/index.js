d91e0181ada4d32c745b72f1d04561d273b3d567
Ammar <ammarbinanwarfuad@gmail.com>
2024-09-24 22:45:12 +0600

    Function to get inner text as a number added

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//totalBalance
+
+//donateButtonNoakhali
+//donateButtonFeni
+//donateButtonQuota
+
+//inputNoakhali
+//inputFeni
+//inputQuota
+
+//donatedNoakhali
+//donatedFeni
+//donatedQuota
+
+console.clear();
+
+//Function to get inner text and convert it into a number
+function getInnerTextById(id) {
+    return parseFloat(document.getElementById(id).innerText) || 0;
+}
